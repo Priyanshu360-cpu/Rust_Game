7fb13c0d8d5589cd8b8fe89116797116d05a9ba9
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-06-25 21:17:16 +0530

    added coords and 2d cams

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,13 @@ enum Display {
 struct Count{
     count: u32,
 }
+
+//DEFINED COORDINATES FOR THE GAME
+const LEFT_BOUND: f32 = -450.0;
+const RIGHT_BOUND: f32 = 450.0;
+const TOP_BOUND: f32 = 300.0;
+const BOTTOM_BOUND: f32 = -300.0;
+
 fn main(){
     App::new()
         .add_plugins(DefaultPlugins)
@@ -34,8 +41,11 @@ fn main(){
         .run();
 }
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
+    //initalizing cameras
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(UiCameraBundle::default());
+
+
 }
 fn processed() {
    
